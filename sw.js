9657c95ba3c01ee22f406f2c90df212a718abe27
a50312945-oss <a50312945@gmail.com>
2025-10-03 20:35:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-// Cache core for offline-ish use
-const CACHE = 'photoppt-mobile-inline-v2';
-const CORE = ['.', './index.html', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', e=> e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener('activate', e=> e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
+const CACHE='photoppt-mobile-fromdesktop-v2';const CORE=['.','./index.html','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
